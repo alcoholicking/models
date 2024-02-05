df7ce295a7875f2c4654737dc93a81c53223381b
Alan Ngo <alanngo7711@gmail.com>
2024-02-05 16:31:49 +0800

    Update FastAPI to prompt

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from model import llm_chain
+from PerfectLlama import PerfectLlama
+import asyncio
+
 
 app = FastAPI()
 
@@ -9,11 +12,22 @@ def read_root():
     return {"Hello": "world"}
 
 
-@app.get("/prompts/{prompt}")
-def give_prompt(prompt: str):
+@app.post("/chat")
+def get_body(request: Request):
+    chain = PerfectLlama("llama2/llama-2-7b-chat-quantized/ggml-model-f16.gguf")
+    request = request.json()
+    message = chain.give_prompt(request)
 
-    # get LLM returned grapes
-    return llm_chain(prompt)["text"]
-    # 
+    return message 
 
-    # return 
+# @app.post("/chat")
+# async def chat(query: Query, response: StreamingResponse):
+#   pass
+
+# async def stream():
+#  async for token in callback_handler:
+#  yield token
+# streaming_iterator = stream()
+# task = asyncio.create_task(agent(query, streaming_iterator))
+# return StreamingResponse(streaming_iterator)
+
